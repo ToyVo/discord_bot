b0e0dcf1c9e7b715b98570a9f99a5d555866f27c
Collin Diekvoss <Collin@Diekvoss.com>
2024-09-22 00:14:27 -0500

    filter

--- src/minecraft.rs
+++ src/minecraft.rs
@@ -24,7 +24,7 @@ pub async fn track_players(state: &AppState) -> Result<(), AppError> {
         .trim()
         .split(',')
         .map(|s| s.trim().to_string())
-        .filter(|s| s.is_empty())
+        .filter(|s| s != "")
         .collect::<Vec<String>>();
 
     tracing::debug!("Players: {:?}", players);
