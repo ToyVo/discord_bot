0ca58b7c25dcc3009f0a40fdae26b5a3f5303132
Collin Diekvoss <Collin@Diekvoss.com>
2024-10-29 15:08:30 -0500

    simplify active state

--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,9 @@ pub mod terraria;
 
 pub async fn systemctl_running<S: AsRef<str>>(service_name: S) -> Result<bool, AppError> {
     let output = Command::new("systemctl")
-        .args(["show", service_name.as_ref()])
+        .args(["show", "-P", "ActiveState", service_name.as_ref()])
         .output()
         .await?;
 
-    let output_str = std::str::from_utf8(&output.stdout)?;
-    match output_str
-        .lines()
-        .find(|line| line.starts_with("ActiveState=")) {
-        Some(state) => {
-            match state.split('=').last().unwrap() {
-                "active" => Ok(true),
-                _ => Ok(false),
-            }
-        }
-        None => Err(AppError::Other(String::from("Could not find ActiveState in systemctl output")))
-    }
+    Ok(std::str::from_utf8(&output.stdout)?.trim() == "active")
 }
